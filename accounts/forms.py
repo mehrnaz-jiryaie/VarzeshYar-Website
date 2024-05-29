7299ebb5f6849897ec8e0d1af0ef964d5f3ddc2e
Mehrnaz Jiryaie <mehrnazjiryaie@gmail.com>
2024-05-29 23:54:30 +0330

    Username uniqueness done.

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,6 +8,14 @@ class AccountCreationForm(forms.ModelForm):
         model = Account
         fields = ['username', 'email', 'password', 'confirm_password']
 
+    def clean_username(self):
+        """Checks if the username is unique."""
+        username = self.cleaned_data.get('username')
+        if Account.objects.filter(username=username).exists():
+            raise ValidationError(
+                'این نام کاربری قبلاً گرفته شده است. لطفا یکی دیگر را وارد کنید.')
+        return username
+
     def clean(self):
         """Checks if the password and confirm password are equal."""
         cleaned_data = super().clean()
@@ -15,9 +23,7 @@ class AccountCreationForm(forms.ModelForm):
         confirm_password = cleaned_data.get('confirm_password')
 
         if password != confirm_password:
-            raise ValidationError(
-                '* پسوردها یکسان نیستند، دوباره تلاش کنید.')
-
+            raise ValidationError('پسوردها یکسان نیستند، دوباره تلاش کنید.')
         return cleaned_data
 
 
